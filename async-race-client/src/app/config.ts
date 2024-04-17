d5d15441f26d817bb2f1be9492b1b48ddc1f6aa6
Aliaksandr Yakubouski <stockupby@gmail.com>
2024-04-17 10:03:28 +0200

    fix: load data by set initial value

--- async-race-client/src/app/config.ts
+++ async-race-client/src/app/config.ts
@@ -5,6 +5,44 @@ const header = 250;
 const tracks = 7;
 const startPage = 1;
 const generateCars = 100;
+const names = [
+  'Toyota',
+  'Honda',
+  'Ford',
+  'Chevrolet',
+  'BMW',
+  'Mercedes-Benz',
+  'Audi',
+  'Lexus',
+  'Subaru',
+  'Volkswagen',
+];
+
+const models = [
+  'Corolla',
+  'Civic',
+  'F-150',
+  'Silverado',
+  '3 Series',
+  'E-Class',
+  'A4',
+  'RX',
+  'Outback',
+  'Golf',
+];
+
+const colors = [
+  '#FF5733',
+  '#fffff',
+  '#3357FF',
+  '#57FF33',
+  '#5733FF',
+  '#FF33A3',
+  '#A333FF',
+  '#fff',
+  '#aaa',
+  '#FFA333',
+];
 
 export default {
   garageUrl,
@@ -14,4 +52,7 @@ export default {
   startPage,
   generateCars,
   winnersUrl,
+  names,
+  models,
+  colors,
 };
--- async-race-client/src/app/services/car-obs.service.ts
+++ async-race-client/src/app/services/car-obs.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Car } from '../interfaces/interfaces';
 import config from '../config';
 
 @Injectable()
@@ -9,7 +8,26 @@ export class CarObsService {
   constructor(private http: HttpClient) {}
 
   getAll$() {
+    // this.set100$();
     const source$: Observable<any> = this.http.get(config.garageUrl);
     return source$;
+  }
+
+  set100$() {
+    const { names, colors } = config;
+    for (let index = 0; index < 100; index++) {
+      const newCar = {
+        name: names.at(this.rand_0_10()),
+        color: colors.at(this.rand_0_10()),
+      };
+      console.log(newCar);
+    }
+  }
+
+  getRandom(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+  }
+  rand_0_10(): number {
+    return Math.random() * (10 - 0) + 0;
   }
 }
